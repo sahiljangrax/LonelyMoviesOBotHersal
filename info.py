4051a08258eb52b832fbca58dfd98e25c5c3cb0f
sahiljangrax <157376542+sahiljangrax@users.noreply.github.com>
2024-02-04 00:19:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,7 @@ STREAMHTO = (environ.get('STREAMHTO', ''))
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6373303244').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002115691976').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '6373303244').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '6373303244').split()]
 auth_channel = environ.get('AUTH_CHANNEL', '')
