02e39da06b142a2eef43f6b8d757211f96b20a02
sahiljangrax <157376542+sahiljangrax@users.noreply.github.com>
2024-02-03 21:38:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -124,7 +124,7 @@ else:
     URL = "https://tg-botz-ksfu.onrender.com".format(FQDN)
 
 # add premium logs channel id
-PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', ''))
+PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002067576779'))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
