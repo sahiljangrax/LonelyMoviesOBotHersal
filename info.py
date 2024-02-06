28036baa079e9f559998feb934eaaa98a00b9071
sahiljangrax <157376542+sahiljangrax@users.noreply.github.com>
2024-02-06 18:51:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -103,8 +103,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://lmsearchbot-e6bae3acfbcb.herokuapp.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://lmsearchbot-e6bae3acfbcb.herokuapp.com".format(FQDN, PORT)
+URL = "https://tg-botz-ksfu.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://tg-botz-ksfu.onrender.com".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -119,9 +119,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
-    URL = "https://lmsearchbot-e6bae3acfbcb.herokuapp.com".format(FQDN)
+    URL = "https://tg-botz-ksfu.onrender.com".format(FQDN)
 else:
-    URL = "https://lmsearchbot-e6bae3acfbcb.herokuapp.com".format(FQDN)
+    URL = "https://tg-botz-ksfu.onrender.com".format(FQDN)
 
 # add premium logs channel id
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002067576779'))
