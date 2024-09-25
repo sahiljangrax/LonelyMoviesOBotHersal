1e0e3c7591a014500ece19c7446de3b0b481dc1b
sahiljangrax <157376542+sahiljangrax@users.noreply.github.com>
2024-09-25 12:07:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -98,13 +98,13 @@ NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
     ON_HEROKU = True
-    APP_NAME = environ.get('sahiljangra')
+    APP_NAME = environ.get('sahiljangrabot')
 else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://ipopcornbot-a7524bbc.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://ipopcornbot-a7524bbc.koyeb.app/".format(FQDN, PORT)
+URL = "https://bloody-sharai-sahiljangrabot-6311c9a1.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://bloody-sharai-sahiljangrabot-6311c9a1.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -119,12 +119,12 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
-    URL = "https://ipopcornbot-a7524bbc.koyeb.app/".format(FQDN)
+    URL = "https://bloody-sharai-sahiljangrabot-6311c9a1.koyeb.app/".format(FQDN)
 else:
-    URL = "https://ipopcornbot-a7524bbc.koyeb.app/".format(FQDN)
+    URL = "https://bloody-sharai-sahiljangrabot-6311c9a1.koyeb.app/".format(FQDN)
 
 # add premium logs channel id
-PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002067576779'))
+PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002175721220'))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
